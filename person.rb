2986a74869afcb63ccffd7f535569aebd878af95
samiullahbahadur <bahadursamiullah@gmail.com>
2022-05-24 10:10:14 +0430

    Add is_of_age method

--- person.rb
+++ person.rb
@@ -3,8 +3,15 @@ class  Person
     attr_accessor :name, :age
 
     def initialize (age, name='unknown', parent_permission: true)
-        
-    end
+        super()
+        @id=random.rand(0..1000)
+        @name=name
+        @age=age
 
+    end
+    def is_of_age 
+        @age >=18
+    end
+    
 
 end
