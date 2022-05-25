f457f630c2ec1bd9fb29dd5b7f648d8b748dd6e2
samiullahbahadur <bahadursamiullah@gmail.com>
2022-05-25 20:45:49 +0430

    Added relationshipt student to classroom

--- student.rb
+++ student.rb
@@ -13,5 +13,10 @@ class Student < Person
   def  classroom =(new_classroom)
     if new_classroom.is_a(Classroom)
       @classroom.remove_student(self) unless @classroom.nil? ||@classroom==new_classroom
-      
+      @classroom = new_classroom
+      @classroom.add_student(self) unless @classroom.students.include?(self)
+    else
+      @classroom = nil
+    end
+  end
 end
