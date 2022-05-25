aaf0fd4bb40c8dfcbadf11eb94452e48c49a6729
samiullahbahadur <bahadursamiullah@gmail.com>
2022-05-25 21:11:32 +0430

    Added constructor

--- rental.rb
+++ rental.rb
@@ -1,5 +1,13 @@
 
 
 class Rental
-    attr_accessor :date, :person, :Book
+    attr_accessor :date, :person, :book
+    def initialize(date, person, book)
+     @date=date
+     @person =person
+     person.retals << self
+     @book=book
+     book.rentals << self
+    end   
+
 end
