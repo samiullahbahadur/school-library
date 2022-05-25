e9ca06dd997b7fd49358b43db02bb2dcc84297d0
samiullahbahadur <bahadursamiullah@gmail.com>
2022-05-25 21:05:09 +0430

    Created rental methods

--- book.rb
+++ book.rb
@@ -1,5 +1,7 @@
+require './rental'
+
 class Book
-  attr_accessor :title, :author
+  attr_accessor :title, :author, :rentals
     def initialize(title, author)
         @title=title
         @author=author
---
+++ rental.rb
@@ -0,0 +1,5 @@
+
+
+class Rental
+    attr_accessor :date, :person, :Book
+end
