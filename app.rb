1852e3fa4037ebd815468c53b9618e60a381fe57
samiullahbahadur <bahadursamiullah@gmail.com>
2022-05-27 11:42:32 +0430

    Added constructor

--- app.rb
+++ app.rb
@@ -1,5 +1,19 @@
 
 class App
-    
+    def initialize 
+        @store ={
+            person: []
+            books: []
+            rentals: []
+        }
+        @method={
+            '1': 'list_all_books',
+            '2': 'list_all_people',
+            '3': 'create_person',
+            '4': 'create_book',
+            '5': 'create_a_rental',
+            '6': 'list_rental_by_person_id',
+            '7': 'exit' 
+        }
 
 end
