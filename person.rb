0189d079f7325eb3fd467e734fb6c4e8fa532f42
samiullahbahadur <bahadursamiullah@gmail.com>
2022-06-01 20:17:37 +0430

    Add people file

--- person.rb
+++ person.rb
@@ -1,31 +1,25 @@
 require './nameable'
-require 'securerandom'
+require './rental'
 
 class Person < Nameable
-  attr_accessor :name, :age, :rentals
-
-  attr_reader :id
-
-  def initialize(age, name = 'unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     super()
-    @id = Random.rand(0..1000)
+    @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
     @rentals = []
-    @id = SecureRandom.uuid
   end
 
-  def of_age
-    @age >= 18
-  end
+  # accessor get method
+  attr_reader :id
 
-  def can_use_services
-    if of_age || parent_permission
-      true
-    else
-      false
-    end
+  # accessor get and set method
+  attr_accessor :name, :age, :rentals
+
+  # Public method
+  def can_use_services?
+    of_age? || @parent_permission
   end
 
   def correct_name
@@ -33,6 +27,14 @@ class Person < Nameable
   end
 
   def add_rental(date, book)
-    Rental.new(date, self, book)
+    @rentals.push(Rental.new(date, book, self)) unless @rentals.include?(Rental.new(date, book, self))
+  end
+
+  # private method
+
+  private
+
+  def of_age?
+    age >= 18
   end
 end
